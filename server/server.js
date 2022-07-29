3e48229e6182f3f3ca1f6a185faf56d31da75fab
devillies <devilliesdev@gmail.com>
2022-07-29 19:27:52 +0700

    add backend structure

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import mongoose from 'mongoose'
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.send('hello world')
+})
+
+app.listen(3500)
