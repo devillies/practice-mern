9d3cb34eed08e31c66d8612fa0fba2d0a58c265c
devillies <devilliesdev@gmail.com>
2022-07-29 19:37:48 +0700

    add user routes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import userRouter from './routes/user.js'
+
 const app = express()
 
 app.use(express.json())
@@ -7,5 +9,7 @@ app.use(express.json())
 app.get('/', (req, res) => {
 	res.send('hello world')
 })
+
+app.use('/user', userRouter)
 
 app.listen(3500)
